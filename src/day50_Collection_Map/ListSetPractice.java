3fb74a07c49c99d5863e1376a6989a90fce10c63
RaikaMogultay <raika.engin@gmail.com>
2022-02-28 10:19:42 -0600

    list and set practice

---
+++ src/day50_Collection_Map/ListSetPractice.java
@@ -0,0 +1,51 @@
+package day50_Collection_Map;
+
+import java.util.*;
+
+public class ListSetPractice {
+    public static void main(String[] args) {
+
+        Set<Integer>set = new HashSet<>();
+        set.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
+
+        System.out.println(set);
+
+        List<Integer> list = new ArrayList<>(set);
+        System.out.println(list);
+
+        List<String> names = null;
+
+
+        System.out.println("-----------------------");
+
+        //pop()
+
+        List<Character> chars = new Stack<>();
+
+        chars.addAll(Arrays.asList('A', 'B', 'C', 'D'));
+
+        System.out.println("chars = " + chars); // [A,B,C,D]
+
+        ((Stack)chars).pop();
+
+        System.out.println("chars = " + chars); // [A,B,C]
+
+        //poll() => FIFO
+
+        List<String> name = new LinkedList<>();
+        name.addAll(Arrays.asList("Raika", "Yasin", "Maya", "Arden"));
+
+        ((LinkedList<String>)name).poll();
+        System.out.println(name);
+
+        ((LinkedList<String>)name).poll();
+        System.out.println(name);
+
+
+
+
+
+
+
+    }
+}
